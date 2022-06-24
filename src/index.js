9614f5942104d5218802e6aec48a2c6ce5402199
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-06-24 02:47:03 +0000

    Hit the api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './styles/main.scss';
+import { addScore, fetchScore } from './modules/structure.js';
+
+fetchScore();
+
+const form = document.querySelector('#add-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nameInput = document.querySelector('#user-name').value.trim();
+  const scoreInput = document.querySelector('#user-score').value;
+  addScore(nameInput, scoreInput);
+  form.reset();
+});
+
+document.querySelector('#refresh').addEventListener('click', () => {
+  fetchScore();
+});
---
+++ src/modules/structure.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+const gameId = 'SGzYdY5RH5N5jim7hk5I';
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const scoreURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SGzYdY5RH5N5jim7hk5I/scores';
+// `${baseURL + gameId}/scores/`;
+
+// Generate game ID
+const createGame = async () => {
+  await fetch(baseURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      name: 'Quami game',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => (json));
+};
+
+const generateTemplate = (scores) => {
+  const listContainer = document.querySelector('.score-list');
+
+  listContainer.innerHTML += `<li>${scores.userName}: ${scores.score}</li>`;
+};
+
+// fetch added score from API
+const fetchScore = async () => {
+  await fetch(scoreURL)
+    .then((response) => response.json())
+    .then((json) => generateTemplate(json.result));
+};
+
+// Push score to API
+const addScore = async (userName, score) => {
+  await fetch(scoreURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      userName, score,
+    }),
+  }).then((response) => response.json()).then((json) => {
+    fetchScore();
+  }).catch((error) => (error));
+};
+
+export { fetchScore, addScore };
